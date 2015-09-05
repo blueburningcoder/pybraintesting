a14845d9654af25e09c3d8359aa66ad953e96b56
blueburningcoder <f.karg10@gmail.com>
2015-09-05 10:33:16 +0200

    added first two files to play with

---
+++ initxordataset.py
@@ -0,0 +1,9 @@
+from pybrain.datasets import SupervisedDataSet
+
+ds = SupervisedDataSet(2, 1)
+
+ds.addSample((0, 0), (0,))
+ds.addSample((0, 1), (1,))
+ds.addSample((1, 0), (1,))
+ds.addSample((1, 1), (0,))
+
---
+++ pybraininit.py
@@ -0,0 +1,4 @@
+# coding: utf-8
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.structure import TanhLayer, SoftmaxLayer
+from pybrain.supervised.trainers import BackpropTrainer

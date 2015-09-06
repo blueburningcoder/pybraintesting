e114f76a058ee1d7c251b45b3d349581d2783978
blueburningcoder <f.karg10@gmail.com>
2015-09-06 11:32:51 +0200

    fixed for ipython, working now

--- pybraininit.py
+++ pybraininit.py
@@ -1,9 +1,8 @@
 # coding: utf-8
 
-def init():
-    from pybrain.tools.shortcuts import buildNetwork
-    from pybrain.structure import TanhLayer
-    from pybrain.supervised.trainers import BackpropTrainer
-    print "pyBrain initiated"
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.structure import TanhLayer
+from pybrain.supervised.trainers import BackpropTrainer
 
-init()
+print "pyBrain initiated"
+

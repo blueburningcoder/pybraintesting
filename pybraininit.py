78548e23c73093faff0f26fd3a13851475c596d1
blueburningcoder <f.karg10@gmail.com>
2015-09-15 09:50:07 +0200

    added some more imports

--- pybraininit.py
+++ pybraininit.py
@@ -4,5 +4,8 @@ from pybrain.tools.shortcuts import buildNetwork
 from pybrain.structure import TanhLayer
 from pybrain.supervised.trainers import BackpropTrainer
 
-print "pyBrain initiated"
 
+
+from pybrain.structure import FeedForwardNetwork
+from pybrain.structure import LinearLayer, SigmoidLayer
+from pybrain.structure import FullConnection
